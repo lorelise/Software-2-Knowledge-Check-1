4abd2a1c247f772181856febcf7cd31f0016aa85
lorelise <brittanylilygren@gmail.com>
2023-06-18 19:05:19 -0400

    Created abstract classes for specifc materials.

---
+++ Software2KnowledgeCheck1/Software2KnowledgeCheck1/Material.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Software2KnowledgeCheck1
+{
+    internal abstract class Material
+    {
+        //each material has a first step that returns a string
+        internal abstract string MaterialConstructionFirstStep();
+    }
+}
